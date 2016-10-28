8b7779eed8048f7d380bcabcb352f80e2cad8696
Hoang Nhat Nguyen <hoangnn93@gmail.com>
2016-10-28 22:52:19 +0700

    :rocket: Initial commit

---
+++ test/promise-misses-errors.js
@@ -0,0 +1,39 @@
+var promise = Promise.resolve();
+
+function throwError() {
+  throw new Error('Ops!');
+}
+
+describe('promise.then(callback).catch(callback)', () => {
+
+  it('catches thrown error', (done) => {
+
+    function passTest() {
+      done();
+    }
+
+    function failTest() {
+      done("failed");
+    }
+
+    promise
+      .then(throwError)
+      .catch(passTest);
+  })
+})
+
+describe('promise.then(callback, callback)', () => {
+
+  it('catches thrown error', (done) => {
+
+    function passTest() {
+      done();
+    }
+
+    function failTest() {
+      done("failed");
+    }
+
+    promise.then(throwError, passTest);
+  })
+})
